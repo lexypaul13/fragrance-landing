76757bf40fb0625498c3ff0f5fdab9524005aea9
Mayowa Paul <lexypaul13@yahoo.com>
2025-02-22 00:21:35 -0500

    chore: ignore ESLint errors during build

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
   },
+  eslint: {
+    ignoreDuringBuilds: true, // This will ignore ESLint errors during build
+  },
 }
 
 module.exports = nextConfig 
